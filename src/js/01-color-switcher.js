f102f6d7dccd07660664bdae1e45a6c4137d5b18
Dmytrii Vitkovskyi <dimavitkovskiy2605@gmail.com>
2023-02-06 17:15:55 +0200

    ready-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+const DELAY = 1000;
+let timeoutId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartChangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  timeoutId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  refs.startBtn.disabled = true;
+}
+
+function onStopChangeColor() {
+  refs.startBtn.disabled = false;
+  clearInterval(timeoutId);
+}
